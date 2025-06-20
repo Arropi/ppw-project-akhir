b905427193869124a0860062ef3f84c8044635a5
Arropi <m.faizarrofi@gmail.com>
2025-06-21 05:54:33 +0700

    delete

--- delete.php
+++ delete.php
@@ -1,11 +1,21 @@
 <?php 
-    require 'config.php';
-    require 'session.php';
-    if($_GET['user_id']){
-        $user_id = $_GET['user_id'];
-        $stid = oci_parse($conn, 'DELETE users_tbl where user_id = :user_id');
-        oci_bind_by_name($stid, ':user_id', $user_id);
-        oci_execute($stid);
-        include_once('logout.php');
+require 'config.php';
+require 'session.php';
+
+if (isset($_GET['user_id'])) {
+    $user_id = (int) $_GET['user_id']; 
+
+    
+    $stmt = mysqli_prepare($koneksi, "DELETE FROM users_tbl WHERE user_id = ?");
+    mysqli_stmt_bind_param($stmt, "i", $user_id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: logout.php");
+        exit;
+    } else {
+        echo "Gagal menghapus user: " . mysqli_error($koneksi);
     }
+
+    mysqli_stmt_close($stmt);
+}
 ?>
